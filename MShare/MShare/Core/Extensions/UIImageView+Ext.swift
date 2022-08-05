8fe6c1063b1c7c83355c4e22eb39fda2cc934d64
Bohdan Pokhidnia <bogdanpoh@icloud.com>
2022-08-05 21:11:11 +0300

    added download and set image on url

--- MShare/MShare/Core/Extensions/UIImageView+Ext.swift
+++ MShare/MShare/Core/Extensions/UIImageView+Ext.swift
@@ -6,12 +6,40 @@
 //
 
 import UIKit
+import Kingfisher
 
 extension UIImageView {
     
     @discardableResult
     func setImage(_ aImage: UIImage?) -> Self {
         image = aImage
+        return self
+    }
+    
+    @discardableResult
+    func setImage(_ urlString: String, placeholder: UIImage?) -> Self {
+        guard let imageURL = URL(string: urlString) else {
+            image = placeholder
+            return self
+        }
+        
+        kf.setImage(with: imageURL, placeholder: placeholder) { [weak self] (result) in
+            switch result {
+            case .success(let imageResult):
+                self?.image = imageResult.image
+                
+            case .failure(let error):
+                self?.image = placeholder
+                
+                print("[dev] error fetch image on url: \(urlString) \(error)")
+            }
+        }
+        return self
+    }
+    
+    @discardableResult
+    func setContentMode(_ contentMode: ContentMode) -> Self {
+        self.contentMode = contentMode
         return self
     }
     
