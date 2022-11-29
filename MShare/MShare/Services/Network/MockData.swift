d6d88de81baaea26f032220cdaadcd2b1afbdee7
Bohdan Pokhidnia <bogdanpoh@icloud.com>
2022-11-29 22:22:24 +0200

    added new available for mock data

--- MShare/MShare/Services/Network/MockData.swift
+++ MShare/MShare/Services/Network/MockData.swift
@@ -24,6 +24,7 @@ extension NetworkService {
                                                      song: .init(songSourceId: "1479105644",
                                                                  songUrl: "https://music.apple.com/ua/album/%D1%82%D0%B2%D1%96%D0%B9-%D0%BD%D0%B0-100/1479103809?i=1479105644",
                                                                  songName: "Твій на 100%",
+//                                                                 songName: "Заведу Кота (Наживо)",
                                                                  artistName: "Boombox",
                                                                  albumName: "Таємний код. Рубікон, Частина 1",
                                                                  coverImageUrl: "https://is3-ssl.mzstatic.com/image/thumb/Music113/v4/3f/83/79/3f8379c8-f8b1-f77e-6dec-6cfcd6d2e740/cover.jpg/640x640bb.jpg",
